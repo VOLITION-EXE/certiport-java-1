43959adf21c5b6efd043734d829558eefc4ed3c3
Vu Nguyen <vunguyen01202009@gmail.com>
2026-05-18 21:20:01 +0000

    67

--- Rectangle.java
+++ Rectangle.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Rectangle
 {
+    private int width;
+    private int length;
+
+    public Rectangle(int w, int l){
+        width = w;
+        length = l;
+    }
+
+    public int perimeter(){
+        return 2*(width+length);
+    }
+
+    public int area(){
+        return width*length;
+    }
     // TO DO: Create attributes for your Rectangle class
 
     
@@ -22,6 +37,10 @@ public class Rectangle
         int grade = Integer.parseInt(scan.nextLine());
 
         System.out.printf("Hello %s! You are in %dth grade!%n", name, grade);
+        double gpa = Double.parseDouble(scan.nextLine());
+        String hs = scan.nextLine();
+
+        System.out.println("your GPA is "+  gpa +" and You are in " + hs);
 
         // TO DO: Add code that reads in a third and fourth line, which are the GPA and school name.
         // Add these fields to your input.txt
@@ -29,6 +48,8 @@ public class Rectangle
 
 
         // TO DO: Create Rectangle objects and compute their perimeter and areas
+        Rectangle rect = new Rectangle(10, 5);
+        System.out.println("Perimeter "+  rect.perimeter() +" Area " + rect.area());
 
 
 
--- Yahyeet.java
+++ Yahyeet.java
@@ -9,12 +9,18 @@ public class Yahyeet
      */
     public static void main(String[] args) throws IOException
     {
+        File f = new File(args[0]);
+        Scanner scan = new Scanner(f);
+        int j = Integer.parseInt(args[1]);
+        int sum = 0;
+        for(int i = 0; i<j ; i++){
+            sum += scan.nextInt();
+        }
+        scan.close();
+        System.out.println("Average: " + sum/j);
         if (args.length >= 2)
         {
             System.out.println("You entered\nargs[0]: " + args[0] + "\nargs[1]: " + args[1]);
-
-
-            // TO DO: Calculate student average scores here
         }
         else
         {
